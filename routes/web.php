a0f08d9d4d0527365378ffdf7c571b5e4a1eab04
Ayu Sintya <gungsintya31@gmail.com>
2025-03-17 13:17:58 +0800

    testing aja

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return JsonResource::make([]);
 });
